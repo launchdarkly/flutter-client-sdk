8e94ffa6be12e744c116b2dc5fabc7608eb666ad
Gavin Whelan <gwhelan@launchdarkly.com>
2020-10-16 13:28:26 -0700

    Better json handling on ios.

--- ios/Classes/SwiftLaunchdarklyFlutterClientSdkPlugin.swift
+++ ios/Classes/SwiftLaunchdarklyFlutterClientSdkPlugin.swift
@@ -10,17 +10,68 @@ public class SwiftLaunchdarklyFlutterClientSdkPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-  func configFrom(dict: Dictionary<String, Any>) -> LDConfig {
+  func configFrom(dict: Dictionary<String, Any?>) -> LDConfig {
     var config = LDConfig(mobileKey: dict["mobileKey"] as! String)
-    config.baseUrl = URL(string: dict["baseUri"] as! String)!
-    config.eventsUrl = URL(string: dict["eventsUri"] as! String)!
-    config.streamUrl = URL(string: dict["streamUri"] as! String)!
+    if let baseUrl = dict["baseUri"] as? String {
+        config.baseUrl = baseUrl
+    }
+    if let eventsUrl = dict["eventsUri"] as? String {
+        config.eventsUrl = eventsUrl
+    }
+    if let streamUrl = dict["streamUri"] as? String {
+        config.streamUrl = streamUrl
+    }
+    if let eventsCapacity = dict["eventsCapacity"] as? Int {
+        config.eventCapacity = eventsCapacity
+    }
+    if let eventsFlushIntervalMillis = dict["eventsFlushIntervalMillis"] as? Int {
+        config.eventFlushInterval = Double(eventsFlushIntervalMillis) / 1000.0
+    }
+    if let connectionTimeoutMillis = dict["connectionTimeoutMillis"] as? Int {
+        config.connectionTimeout = Double(connectionTimeoutMillis) / 1000.0
+    }
+    if let pollingIntervalMillis = dict["pollingIntervalMillis"] as? Int {
+        config.flagPollingInterval = Double(pollingIntervalMillis) / 1000.0
+    }
+    if let backgroundPollingIntervalMillis = dict["backgroundPollingIntervalMillis"] as? Int {
+        config.backgroundFlagPollingInterval = Double(backgroundPollingIntervalMillis) / 1000.0
+    }
+    if let diagnosticRecordingIntervalMillis = dict["diagnosticRecordingIntervalMillis"] as? Int {
+        config.diagnosticRecordingInterval = Double(diagnosticRecordingIntervalMillis) / 1000.0
+    }
+    if let stream = dict["stream"] as? Bool {
+        config.streamingMode = stream ? LDStreamingMode.streaming : LDStreamingMode.polling
+    }
+    if let offline = dict["offline"] as? Bool {
+        config.startOnline = !offline
+    }
+    if let disableBackgroundUpdating = dict["disableBackgroundUpdating"] as? Bool {
+        config.enableBackgroundUpdates = !disableBackgroundUpdating
+    }
+    if let useReport = dict["useReport"] as? Bool {
+        config.useReport = useReport
+    }
+    if let inlineUsersInEvents = dict["inlineUsersInEvents"] as? Bool {
+        config.inlineUserInEvents = inlineUsersInEvents
+    }
+    if let evaluationReasons = dict["evaluationReasons"] as? Bool {
+        config.evaluationReasons = evaluationReasons
+    }
+    if let diagnosticOptOut = dict["diagnosticOptOut"] as? Bool {
+        config.diagnosticOptOut = diagnosticOptOut
+    }
+    if let allAttributesPrivate = dict["allAttributesPrivate"] as? Bool {
+        config.allUserAttributesPrivate = allAttributesPrivate
+    }
+    if let privateAttributeNames = dict["privateAttributeNames"] as [Any] {
+        config.privateUserAttributes = privateAttributeNames.compactMap { $0 as? String }
+    }
     config.wrapperName = "FlutterClientSdk"
     // TODO wrapperVersion
     return config
   }
 
-  func userFrom(dict: Dictionary<String, Any>) -> LDUser {
+  func userFrom(dict: Dictionary<String, Any?>) -> LDUser {
     var user = LDUser(key: dict["key"] as? String)
     if let anonymous = dict["anonymous"] as? Bool { user.isAnonymous = anonymous }
     user.secondary = dict["secondary"] as? String
@@ -50,21 +101,32 @@ public class SwiftLaunchdarklyFlutterClientSdkPlugin: NSObject, FlutterPlugin {
     case "track":
       try! LDClient.get()!.track(key: args?["eventName"] as! String)
     case "boolVariation":
-      let evalResult: Bool? = LDClient.get()!.variation(forKey: args?["flagKey"] as! String,
-                                                        defaultValue: args?["fallback"] as? Bool)
-      result(evalResult)
+      result(LDClient.get()!.variation(forKey: args?["flagKey"] as! String, defaultValue: args?["defaultValue"] as? Bool))
     case "intVariation":
-      let evalResult: Int? = LDClient.get()!.variation(forKey: args?["flagKey"] as! String,
-                                                       defaultValue: args?["fallback"] as? Int)
-      result(evalResult)
+      result(LDClient.get()!.variation(forKey: args?["flagKey"] as! String, defaultValue: args?["defaultValue"] as? Int))
     case "doubleVariation":
-      let evalResult: Double? = LDClient.get()!.variation(forKey: args?["flagKey"] as! String,
-                                                          defaultValue: args?["fallback"] as? Double)
-      result(evalResult)
+      result(LDClient.get()!.variation(forKey: args?["flagKey"] as! String, defaultValue: args?["defaultValue"] as? Double))
     case "stringVariation":
-      let evalResult: String? = LDClient.get()!.variation(forKey: args?["flagKey"] as! String,
-                                                          defaultValue: args?["fallback"] as? String)
-      result(evalResult)
+      result(LDClient.get()!.variation(forKey: args?["flagKey"] as! String, defaultValue: args?["defaultValue"] as? String))
+    case "jsonVariation":
+      let flagKey = args?["flagKey"] as! String
+      if let defaultValue = args["defaultValue"] as? Bool {
+        result(LDClient.get()!.variation(forKey: flagKey, defaultValue: defaultValue))
+      } else if let defaultValue = args["defaultValue"] as? Int {
+        result(LDClient.get()!.variation(forKey: flagKey, defaultValue: defaultValue))
+      } else if let defaultValue = args["defaultValue"] as? Double {
+        result(LDClient.get()!.variation(forKey: flagKey, defaultValue: defaultValue))
+      } else if let defaultValue = args["defaultValue"] as? String {
+        result(LDClient.get()!.variation(forKey: flagKey, defaultValue: defaultValue))
+      } else if let defaultValue = args["defaultValue"] as? [Any] {
+        result(LDClient.get()!.variation(forKey: flagKey, defaultValue: defaultValue))
+      } else if let defaultValue = args["defaultValue"] as? [String: Any] {
+        result(LDClient.get()!.variation(forKey: flagKey, defaultValue: defaultValue))
+      } else {
+        result(nil)
+      }
+    case "allFlags":
+        result(LDClient.get()!.allFlags)
     case "setOnline":
       let online: Bool? = args?["online"] as? Bool
       if let online = online {
@@ -74,7 +136,8 @@ public class SwiftLaunchdarklyFlutterClientSdkPlugin: NSObject, FlutterPlugin {
     case "flush":
       LDClient.get()!.flush()
       result(nil)
-    default: return
+    default:
+      result(FlutterMethodNotImplemented)
     }
   }
 }
