aa98c56fafec6fe83514a26f1d622d8715c6fd84
Gavin Whelan <gwhelan@launchdarkly.com>
2022-02-04 15:08:39 -0600

    Fix CI job (#26)

--- .ldrelease/prepare.sh
+++ .ldrelease/prepare.sh
@@ -2,19 +2,26 @@
 
 set -e
 
-# Setup PATH
+FLUTTER_RELEASE="https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_2.8.1-stable.zip"
+
+# Setup Android PATHs
 echo 'export PATH="$PATH:/usr/local/share/android-sdk/tools/bin"' >> $BASH_ENV
 echo 'export PATH="$PATH:/usr/local/share/android-sdk/platform-tools"' >> $BASH_ENV
-echo 'export PATH="$PATH:$HOME/.pub-cache/bin"' >> $BASH_ENV
 
-# Install Android SDK, Flutter, and dartdoc
-HOMEBREW_NO_AUTO_UPDATE=1 brew tap homebrew/cask
-HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask homebrew/cask-versions/adoptopenjdk8
-HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask android-sdk
+# Install Java and Android SDK
+brew install --cask temurin8 android-sdk
 yes | sdkmanager "platform-tools" | grep -v = || true
-HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask flutter
-flutter upgrade
-dart pub global activate dartdoc
 
 # Acknowledge Android licenses
 sudo mkdir -p /usr/local/android-sdk-linux/licenses
+
+# Install Flutter and dartdoc
+cd $HOME
+curl $FLUTTER_RELEASE -o flutter.zip
+unzip -q flutter.zip
+export PATH="$PATH:$HOME/flutter/bin"
+dart pub global activate dartdoc
+
+# Setup Flutter PATHs
+FLUTTER_PATHS="$HOME/flutter/bin:$HOME/.pub-cache/bin"
+echo "export PATH=\"\$PATH:$FLUTTER_PATHS\"" >> $BASH_ENV
