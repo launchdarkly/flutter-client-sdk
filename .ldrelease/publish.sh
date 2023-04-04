54c08dd4bfe922bee26625c7e2f821ac0bb1a466
Todd Anderson <tanderson@launchdarkly.com>
2023-04-04 13:51:35 -0500

    Escaping mac path

--- .ldrelease/publish.sh
+++ .ldrelease/publish.sh
@@ -5,8 +5,8 @@ set -e
 # Copy secret to allow publishing
 if [ "$(uname)" == "Darwin" ]; then
 	# Mac
-	mkdir -p ~/Library/Application Support/dart/
-	mv $LD_RELEASE_SECRETS_DIR/pub_creds ~/Library/Application Support/dart/pub-credentials.json
+	mkdir -p ~/Library/Application\ Support/dart
+	mv $LD_RELEASE_SECRETS_DIR/pub_creds ~/Library/Application\ Support/dart/pub-credentials.json
 else
 	# Linux without XDG_CONFIG_HOME defined.
 	mkdir -p ~/.config/dart
