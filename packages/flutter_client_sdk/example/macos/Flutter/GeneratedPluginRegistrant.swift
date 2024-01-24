588ae0179871c470442ab9ec2d6c03a1754f103e
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-01-24 14:56:20 -0800

    feat: Add example project. (#93)
    Adds a basic example project.
    
    The main areas of focus are `lib/main.dart`, the pubspec, and the
    README.md. Everything else is generated. It is using the default
    generate gitignore files.

---
+++ packages/flutter_client_sdk/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import device_info_plus
+import package_info_plus
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
