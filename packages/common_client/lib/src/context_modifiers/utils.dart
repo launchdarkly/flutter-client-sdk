e93b0b34e1e537323ae2b3aaddd555794ae73598
Todd Anderson <tanderson@launchdarkly.com>
2024-01-29 14:01:23 -0600

    fix: adjusting documentation on getOrGenerateKey

--- packages/common_client/lib/src/context_modifiers/utils.dart
+++ packages/common_client/lib/src/context_modifiers/utils.dart
@@ -1,7 +1,7 @@
 import 'package:uuid/uuid.dart';
 import '../persistence/persistence.dart';
 
-/// Retrieves the key for the given [kind] from [persistence] if it exists.  If
+/// Retrieves the key from [persistence] for the given [namespace] and [kind] if it exists.  If
 /// one does not exist, generates a key, saves it, and returns it.
 Future<String> getOrGenerateKey(Persistence persistence, String namespace, String kind) async {
   final encodedKind = encodePersistenceKey(kind);
