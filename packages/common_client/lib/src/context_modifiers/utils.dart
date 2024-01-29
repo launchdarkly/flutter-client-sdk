0f3e9a19fc686efa5f4c1d9e5208d0ab3858689b
Todd Anderson <tanderson@launchdarkly.com>
2024-01-29 13:58:39 -0600

    fix: anonymous context keys and auto env context keys are now in separate namespaces

--- packages/common_client/lib/src/context_modifiers/utils.dart
+++ packages/common_client/lib/src/context_modifiers/utils.dart
@@ -1,17 +1,15 @@
 import 'package:uuid/uuid.dart';
 import '../persistence/persistence.dart';
 
-const _generatedKeyNamespace = 'LaunchDarkly_GeneratedContextKeys';
-
 /// Retrieves the key for the given [kind] from [persistence] if it exists.  If
 /// one does not exist, generates a key, saves it, and returns it.
-Future<String> getOrGenerateKey(Persistence persistence, String kind) async {
+Future<String> getOrGenerateKey(Persistence persistence, String namespace, String kind) async {
   final encodedKind = encodePersistenceKey(kind);
-  final stored = await persistence.read(_generatedKeyNamespace, encodedKind);
+  final stored = await persistence.read(namespace, encodedKind);
   if (stored != null) {
     return stored;
   }
   final newKey = Uuid().v4();
-  await persistence.set(_generatedKeyNamespace, encodedKind, newKey);
+  await persistence.set(namespace, encodedKind, newKey);
   return newKey;
 }
