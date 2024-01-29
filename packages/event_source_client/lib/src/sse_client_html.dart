3e003d4f3fe69a337872f2f081956fbc91d9cc64
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-01-29 13:03:53 -0800

    Implement backoff in web client.

--- packages/event_source_client/lib/src/sse_client_html.dart
+++ packages/event_source_client/lib/src/sse_client_html.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:html' as html;
+import 'dart:math' as math;
 
 import '../launchdarkly_event_source_client.dart';
 
+import 'backoff.dart';
 import 'message_event.dart' as ld_message_event;
 
 /// An [SSEClient] that uses the [html.EventSource] available on most browsers for web platform support.
@@ -14,24 +16,55 @@ class HtmlSseClient implements SSEClient {
   late final StreamController<ld_message_event.MessageEvent>
       _messageEventsController;
 
+  Backoff _backoff = Backoff(math.Random());
+
+  final Uri _uri;
+  final Set<String> _eventTypes;
+
+  int? _activeSince;
+  Timer? _retryTimer;
+
   /// Creates an instance of an SSEClient that will connect in the future
   /// to the [uri] provided.
-  HtmlSseClient(Uri uri, Set<String> eventTypes) {
+  HtmlSseClient(Uri uri, Set<String> eventTypes)
+      : _uri = uri,
+        _eventTypes = eventTypes {
     _messageEventsController =
         StreamController<ld_message_event.MessageEvent>.broadcast(
       onListen: () {
         // this is triggered when first listener subscribes
-        _eventSource = html.EventSource(uri.toString());
-        for (var eventType in eventTypes) {
-          _eventSource?.addEventListener(eventType, _handleMessageEvent);
-        }
+        _setupConnection();
       },
       onCancel: () {
         // this is triggered when last listener unsubscribes
-        _eventSource?.close();
-        _eventSource = null;
+        _retryTimer?.cancel();
+        _closeConnection();
       },
     );
+  }
+
+  void _closeConnection() {
+    _eventSource?.close();
+    _eventSource = null;
+  }
+
+  void _setupConnection() {
+    // this is triggered when first listener subscribes
+    _eventSource = html.EventSource(_uri.toString());
+    // Reset the backoff data whenever the client consumer
+    // a new connection.
+    _backoff = Backoff(math.Random());
+    for (var eventType in _eventTypes) {
+      _eventSource?.addEventListener(eventType, _handleMessageEvent);
+    }
+    _eventSource?.addEventListener('error', _handleError);
+  }
+
+  void _handleError(html.Event event) {
+    // The browser event source errors are reasonably opaque, if we could
+    // determine the type of condition, then this is where we would
+    // determine if this was a temporary or permanent failure.
+    restart();
   }
 
   void _handleMessageEvent(html.Event event) {
@@ -52,7 +85,17 @@ class HtmlSseClient implements SSEClient {
 
   @override
   void restart() {
-    // TODO: implement restart
+    _closeConnection();
+    final delay = _backoff.getRetryDelay(_activeSince);
+    _activeSince = null;
+
+    // Another retry could be in progress, in which case we just allow it to
+    // continue.
+    // For instance a manual restart overlapping with an error.
+    _retryTimer ??= Timer(Duration(milliseconds: delay), () {
+      _retryTimer = null;
+      _setupConnection();
+    });
   }
 }
 
