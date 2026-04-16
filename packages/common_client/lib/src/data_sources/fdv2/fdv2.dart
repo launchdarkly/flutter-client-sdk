d2cc8de5926b69d86f17fbeebcde329a64ba3e14
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2026-04-16 16:43:04 -0700

    refactor: remove AsyncQueue in favor of idiomatic Dart Streams
    AsyncQueue is a JavaScript pattern. Dart's StreamController/Stream is
    the idiomatic equivalent and is already used by the existing FDv1
    data sources in this codebase. Future FDv2 data sources will use
    StreamController<FDv2SourceResult> instead.

--- packages/common_client/lib/src/data_sources/fdv2/fdv2.dart
+++ packages/common_client/lib/src/data_sources/fdv2/fdv2.dart
@@ -1,7 +1,6 @@
 /// FDv2 protocol and data model types.
 library;
 
-export 'async_queue.dart';
 export 'fdv2_flag_eval_mapper.dart';
 export 'fdv2_payload.dart';
 export 'fdv2_protocol_handler.dart';
